0f5b860864fb50f2207f940d47fc5cc9b9ff3711
Julia <frekkoland@gmail.com>
2018-07-26 20:40:48 +0300

    Adorable Points Tool

---
+++ Assets/Editor/SelectAllPoints.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using SimpleJson;
+
+public class SelectAllPoints : ScriptableWizard
+{
+    private List<WrapPoint> listPoint = new List<WrapPoint>();
+
+    [MenuItem("Tools/Save Points")]
+    static void SellectAllPointsTool()
+    {
+        ScriptableWizard.DisplayWizard<SelectAllPoints>("Save Points", "Select/Serialization");
+    }
+    void OnWizardCreate()
+    {
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+        GameObject[] allPoints = allObjects.Where(x => x.GetComponent(typeof(Point)) != null).ToArray();
+        Selection.objects = allPoints;
+        Serialization(allPoints);
+    }
+
+    void Serialization(GameObject[] objectPoints)
+    {
+        foreach (var objPoint in objectPoints)
+        {
+            WrapPoint newWrapPoint = new WrapPoint(objPoint);
+            Debug.Log(newWrapPoint.x);
+            listPoint.Add(newWrapPoint);
+        }
+
+        string json = JsonHelper.ToJson<WrapPoint>(listPoint.ToArray(), true);
+        //Debug.Log(json);
+        var path = EditorUtility.SaveFilePanel("Serialize Points to JSON", "", "Serialization.json", "json");
+        File.WriteAllLines(path, new[] { json });
+    }
+
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+
+    public float Param1;
+    public float Param2;
+
+    public float x { get { return transform.position.x; } }
+    public float y { get { return transform.position.y; } }
+    public float z { get { return transform.position.z; } }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+[Serializable]
+public class WrapPoint
+{
+
+    public float WrapParam1;
+    public float WrapParam2;
+    public float x;
+    public float y;
+    public float z;
+
+    public WrapPoint(GameObject pointToWrap)
+    {
+        this.WrapParam1 = pointToWrap.GetComponentInChildren<Point>().Param1;
+        this.WrapParam2 = pointToWrap.GetComponentInChildren<Point>().Param2;
+        this.x = pointToWrap.GetComponentInChildren<Point>().x;
+        this.y = pointToWrap.GetComponentInChildren<Point>().y;
+        this.z = pointToWrap.GetComponentInChildren<Point>().z;
+    }
+}
+
+
+
+
