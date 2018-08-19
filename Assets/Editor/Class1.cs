1d4b100aa46549ceb469e57ca43de8ad1ed5d33a
Julia <frekkoland@gmail.com>
2018-08-20 02:13:34 +0300

    mimimi

---
+++ Assets/Editor/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+namespace Assets.Editor
+{
+    class SpawnPointInfo
+    {
+        public GameObject Spawn { get; set; }
+        public float DistanceSum { get; set; }
+        public float SightedPlayerCount { get; set; }
+    }
+}
